4fada9e2282c1f4b9ab76cbdcf68f6e66215b2bf
Jorden Rodriguez <jorden@Jordens-Mac-mini.local>
2024-01-18 17:22:33 -0600

    overhaul2

--- site/next.config.js
+++ site/next.config.js
@@ -3,12 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = {
-  nextConfig,
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-};
+// module.exports = {
+//   nextConfig,
+//   eslint: {
+//     // Warning: This allows production builds to successfully complete even if
+//     // your project has ESLint errors.
+//     ignoreDuringBuilds: true,
+//   },
+// };
 module.exports = nextConfig;
